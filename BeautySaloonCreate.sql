7baaaa4516462c6c429566473237f24d4710080e
Arslan <ArsBurs@gmail.com>
2018-04-23 16:29:08 +0300

    Varchar variables extended, create table request changed, inserts added

--- BeautySaloonCreate.sql
+++ BeautySaloonCreate.sql
@@ -1,160 +1,127 @@
-USE [master]
+USE [Master]
 GO
-/****** Object:  Database [BeautySaloon]    Script Date: 22.04.2018 16:51:37 ******/
-CREATE DATABASE [BeautySaloon]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'BeautySaloon', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\BeautySaloon.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'BeautySaloon_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\BeautySaloon_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+CREATE DATABASE beauty_saloon;
 GO
-ALTER DATABASE [BeautySaloon] SET COMPATIBILITY_LEVEL = 140
+USE beauty_saloon;
 GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [BeautySaloon].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [BeautySaloon] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET AUTO_CLOSE OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [BeautySaloon] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [BeautySaloon] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET  DISABLE_BROKER 
-GO
-ALTER DATABASE [BeautySaloon] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [BeautySaloon] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET RECOVERY SIMPLE 
-GO
-ALTER DATABASE [BeautySaloon] SET  MULTI_USER 
-GO
-ALTER DATABASE [BeautySaloon] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [BeautySaloon] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [BeautySaloon] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [BeautySaloon] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-ALTER DATABASE [BeautySaloon] SET DELAYED_DURABILITY = DISABLED 
-GO
-ALTER DATABASE [BeautySaloon] SET QUERY_STORE = OFF
-GO
-USE [BeautySaloon]
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET IDENTITY_CACHE = ON;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET MAXDOP = PRIMARY;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET PARAMETER_SNIFFING = PRIMARY;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
-GO
-ALTER DATABASE [BeautySaloon] SET  READ_WRITE 
-GO
-
 CREATE TABLE employees
 (  
    employeeID int IDENTITY (1,1) NOT NULL,
-   surname varchar(30) NOT NULL,
-   name varchar(30) NOT NULL,
-   lastname varchar(30) NOT NULL,
+   surname varchar(50) NOT NULL,
+   name varchar(50) NOT NULL,
+   lastname varchar(50) NOT NULL,
    phone_number varchar(20) NOT NULL,
    address varchar(50) NOT NULL,
-   specialization varchar(30) NOT NULL,
+   specialization varchar(100) NOT NULL,
    start_date date NOT NULL,
    CONSTRAINT PK_employees_employeeID PRIMARY KEY CLUSTERED (employeeID)
 );
-
 CREATE TABLE services
 (  
    serviceID int IDENTITY (1,1) NOT NULL,
-   name varchar(30) NOT NULL,
+   name varchar(50) NOT NULL,
    price int NOT NULL,
    comment text,
    CONSTRAINT PK_services_serviceID PRIMARY KEY CLUSTERED (serviceID)
 );
-
 CREATE TABLE clients
 (  
    clientID int IDENTITY (1,1) NOT NULL,
-   surname varchar(30),
-   name varchar(30) NOT NULL,
+   surname varchar(50),
+   name varchar(50) NOT NULL,
    phone_number varchar(20) NOT NULL,
    CONSTRAINT PK_clients_clientID PRIMARY KEY CLUSTERED (clientID)
 );
-
 CREATE TABLE records
 (  
    recordID int IDENTITY (1,1) NOT NULL,
-   client_id int FOREIGN KEY REFERENCES clients(clientID),
-   employee_id int FOREIGN KEY REFERENCES employees(employeeID),
-   service_id int FOREIGN KEY REFERENCES services(serviceID),
+   client_id int FOREIGN KEY REFERENCES clients(clientID) NOT NULL,
+   employee_id int FOREIGN KEY REFERENCES employees(employeeID) NOT NULL,
+   service_id int FOREIGN KEY REFERENCES services(serviceID) NOT NULL,
    date date NOT NULL,
    time time NOT NULL,
    comment text,
    CONSTRAINT PK_records_recordID PRIMARY KEY CLUSTERED (recordID)
 );
-
 CREATE TABLE shares
 (  
    shareID int IDENTITY (1,1) NOT NULL,
-   name varchar(30) NOT NULL,
+   name varchar(100) NOT NULL,
    start_date date NOT NULL,
    expiration_date date,
    CONSTRAINT PK_shares_shareID PRIMARY KEY CLUSTERED (shareID)
 );
-
 CREATE TABLE discounts
 (  
    discountID int IDENTITY (1,1) NOT NULL,
-   share_id int FOREIGN KEY REFERENCES shares(shareID),
-   service_id int FOREIGN KEY REFERENCES services(serviceID),
+   share_id int FOREIGN KEY REFERENCES shares(shareID) NOT NULL,
+   service_id int FOREIGN KEY REFERENCES services(serviceID) NOT NULL,
    value int NOT NULL,
    CONSTRAINT PK_discounts_discountID PRIMARY KEY CLUSTERED (discountID)
 );
+/*employees*/
+INSERT INTO employees VALUES('Александрова', 'Александра', 'Александровна', '+79875554141', 'Татарстан, Казань', 'Специалист по мужским стрижкам', '2018-01-01');
+INSERT INTO employees VALUES('Бирушина', 'Соня', 'Самуиловна', '+79875554141', 'Татарстан, Казань', 'Специалист по окрашиванию волос', '2018-02-03');
+INSERT INTO employees VALUES('Мозайкина', 'Татьяна', 'Борисовна', '+79875554141', 'Татарстан, Казань', 'Специалист по женским стрижкам', '2017-12-15');
+INSERT INTO employees VALUES('Ульянова', 'Мария', 'Алексеевна', '+79875554141', 'Татарстан, Казань', 'Специалист по женским и мужским стрижкам', '2018-01-09');
+INSERT INTO employees VALUES('Шарафина', 'Марина', 'Дмитриевна', '+79875554141', 'Татарстан, Казань', 'Специалист по мэйкапу', '2018-01-06');
+INSERT INTO employees VALUES('Осипова', 'Надежда', 'Евдосеевна', '+79875554141', 'Татарстан, Казань', 'Специалист по маникюру', '2018-02-04');
+INSERT INTO employees VALUES('Пирогова', 'Ксения', 'Константиновна', '+79875554141', 'Татарстан, Казань', 'Специалист по мэйкапу', '2018-01-24');
+INSERT INTO employees VALUES('Дебошина', 'Варвара', 'Евгеньевна', '+79875554141', 'Татарстан, Казань', 'Специалист по окрашиванию волос', '2018-01-18');
+INSERT INTO employees VALUES('Каморина', 'Дарья', 'Ульяновна', '+79875554141', 'Татарстан, Казань', 'Администратор', '2017-12-25');
+INSERT INTO employees VALUES('Хасанова', 'Юлия', 'Леонидовна', '+79875554141', 'Татарстан, Казань', 'Специалист по маникюру', '2017-12-10');
+/*services*/
+INSERT INTO services VALUES('Женская стрижка', 500, 'Цена варьируется от 400 до 600 в зависимости от количества работы специалиста');
+INSERT INTO services VALUES('Мужская стрижка', 300, 'Цена варьируется от 200 до 400 в зависимости от количества работы специалиста');
+INSERT INTO services VALUES('Маникюр', 500, '');
+INSERT INTO services VALUES('Мэйкап', 200, 'Цена варьируется от 200 до 400 в зависимости от количества используемой косметики');
+INSERT INTO services VALUES('Окрашивание волос', 300, '');
+/*clients*/
+INSERT INTO clients VALUES('Аистов', 'Кирилл', '+79875554141');
+INSERT INTO clients VALUES('Аллаяров', 'Руслан', '+79875554141');
+INSERT INTO clients VALUES('Анисеня', 'Иван', '+79875554141');
+INSERT INTO clients VALUES('Байдин', 'Никита', '+79875554141');
+INSERT INTO clients VALUES('Басова', 'Александра', '+79875554141');
+INSERT INTO clients VALUES('Воротников', 'Алексей', '+79875554141');
+INSERT INTO clients VALUES('Гордеева', 'Ангелина', '+79875554141');
+INSERT INTO clients VALUES('Горелова', 'Анна', '+79875554141');
+INSERT INTO clients VALUES('Демидова', 'Анна', '+79875554141');
+INSERT INTO clients VALUES('Кондратенков', 'Борис', '+79875554141');
+INSERT INTO clients VALUES('Корчагин', 'Никита', '+79875554141');
+INSERT INTO clients VALUES('Манкевич', 'Павел', '+79875554141');
+INSERT INTO clients VALUES('Нерсисян', 'Степан', '+79875554141');
+INSERT INTO clients VALUES('Новосад', 'Виктор', '+79875554141');
+INSERT INTO clients VALUES('Петрова', 'Александра', '+79875554141');
+INSERT INTO clients VALUES('Попов', 'Юрий', '+79875554141');
+INSERT INTO clients VALUES('Рыхова', 'Александра', '+79875554141');
+INSERT INTO clients VALUES('Дергач', 'Петр', '+79875554141');
+INSERT INTO clients VALUES('Алешин', 'Антон', '+79875554141');
+INSERT INTO clients VALUES('Алексеев', 'Александр', '+79875554141');
+/*records*/
+INSERT INTO records VALUES(5, 2, 5, '2017-03-01', '11:30', '');
+INSERT INTO records VALUES(1, 1, 2, '2017-03-01', '12:30', 'Может опоздать на 10-15 минут');
+INSERT INTO records VALUES(16, 2, 1, '2017-03-01', '13:30', '');
+INSERT INTO records VALUES(6, 4, 4, '2017-03-02', '10:30', '');
+INSERT INTO records VALUES(7, 5, 3, '2017-03-01', '14:30', 'Заросший(ая)');
+INSERT INTO records VALUES(9, 8, 5, '2017-03-01', '15:30', 'Странный человек, оплату брать заранее');
+INSERT INTO records VALUES(19, 7, 1, '2017-03-02', '11:30', '');
+INSERT INTO records VALUES(3, 6, 3, '2017-03-02', '12:30', '');
+INSERT INTO records VALUES(12, 2, 4, '2017-03-02', '13:30', 'Друг подруги, относиться бережно');
+INSERT INTO records VALUES(10, 1, 2, '2017-03-02', '14:30', '');
+/*shares*/
+INSERT INTO shares VALUES('Семейная среда', '2017-01-01', '');
+INSERT INTO shares VALUES('Возраст не помеха', '2017-02-01', '2018-05-01');
+INSERT INTO shares VALUES('Дети - радость жизни', '2017-02-01', '2018-05-01');
+INSERT INTO shares VALUES('Лысый, не горюй', '2017-01-01', '2018-12-31');
+/*discounts*/
+INSERT INTO discounts VALUES(4, 1, 70);
+INSERT INTO discounts VALUES(4, 2, 70);
+INSERT INTO discounts VALUES(1, 1, 15);
+INSERT INTO discounts VALUES(1, 2, 15);
+INSERT INTO discounts VALUES(2, 1, 30);
+INSERT INTO discounts VALUES(2, 2, 30);
+INSERT INTO discounts VALUES(2, 4, 30);
+INSERT INTO discounts VALUES(2, 5, 30);
+INSERT INTO discounts VALUES(3, 1, 10);
+INSERT INTO discounts VALUES(3, 2, 10);
+INSERT INTO discounts VALUES(3, 5, 10);
